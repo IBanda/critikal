ba349b9870d59f58d58baf2703839bca0501a17c
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-08 01:20:15 +0800

    use name in email

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,16 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+
 import transporter from '../../lib/transporter';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     receiverEmail,
     senderEmail,
+    name,
     subject,
     htmlMessage,
     textMessage,
   } = req.body;
   try {
-    await transporter.sendMail({
+    await transporter(name).sendMail({
       to: receiverEmail,
       subject,
       text: textMessage,
