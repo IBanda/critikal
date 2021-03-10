f850a82721107ed722ccd2d256a3b52eaae8da12
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-10 22:24:50 +0800

    add select table filter

---
+++ components/SelectFilter.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+
+export default function SelectFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  const options = useMemo(() => {
+    const opts = new Set();
+    preFilteredRows.forEach((row) => {
+      opts.add(row.values[id]);
+    });
+
+    return [...opts];
+  }, [id, preFilteredRows]);
+
+  return (
+    <select
+      className="bg-gray-100 text-black my-2 px-2 rounded py-1 w-full focus:outline-none focus:ring-2 focus:border-indigo-300"
+      value={filterValue}
+      onChange={(e) => setFilter(e.target.value || undefined)}
+    >
+      <option value="">All</option>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
