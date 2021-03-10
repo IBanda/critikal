712e054e536ffb96615fc365473104cbee269c8f
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-10 22:24:22 +0800

    add text input table filter

---
+++ components/FilterInput.tsx
@@ -0,0 +1,14 @@
+import { SearchIcon } from '@primer/octicons-react';
+
+export default function FilterInput({ column: { filterValue, setFilter } }) {
+  return (
+    <div className="relative my-2 text-black">
+      <SearchIcon className="absolute  top-1/2 left-2 transform -translate-y-1/2 text-indigo-400" />
+      <input
+        className="bg-gray-100 rounded focus:outline-none focus:ring-2 focus:border-indigo-300 py-1 pl-7"
+        value={filterValue || ''}
+        onChange={(e) => setFilter(e.target.value) || undefined}
+      />
+    </div>
+  );
+}
