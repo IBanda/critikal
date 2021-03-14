5764c73b314a8331be40e33bc5123d33e6a7cd0e
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-14 13:03:29 +0800

    add support for DELETE and PATCH verbs

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -6,58 +6,84 @@ import transporter from 'lib/transporter';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (req.method === 'POST') {
-      const {
-        id,
-        receiverEmail,
-        senderEmail,
-        name,
-        subject,
-        htmlMessage,
-        textMessage,
-      } = req.body;
-      const textInsights = await analyzeText(textMessage);
-      const isHigh = textInsights.priority === 'high';
-      const messageId = id + Date.now();
+    await db();
+    switch (req.method) {
+      case 'POST': {
+        const {
+          id,
+          receiverEmail,
+          senderEmail,
+          name,
+          subject,
+          htmlMessage,
+          textMessage,
+        } = req.body;
+        const textInsights = await analyzeText(textMessage);
+        const isHigh = textInsights.priority === 'high';
+        const messageId = id + Date.now();
 
-      await transporter(name).sendMail({
-        to: receiverEmail,
-        subject,
-        text: textMessage,
-        html: htmlMessage,
-        replyTo: senderEmail,
-        messageId,
-        headers: {
-          'x-priority': isHigh ? '1' : '3',
-          'x-msmail-priority': isHigh ? 'High' : 'Normal',
-          importance: isHigh ? 'high' : 'normal',
-        },
-      });
+        await transporter(name).sendMail({
+          to: receiverEmail,
+          subject,
+          text: textMessage,
+          html: htmlMessage,
+          replyTo: senderEmail,
+          messageId,
+          headers: {
+            'x-priority': isHigh ? '1' : '3',
+            'x-msmail-priority': isHigh ? 'High' : 'Normal',
+            importance: isHigh ? 'high' : 'normal',
+          },
+        });
 
-      await db();
-      await Email.create({
-        id: messageId,
-        name,
-        emailId: id,
-        subject,
-        message: htmlMessage,
-        senderEmail,
-        insights: {
-          sentiment: textInsights.sentiment,
-          keyPhrases: textInsights.keyPhrases,
-          priority: textInsights.priority,
-        },
-      });
-      res.status(200).json({
-        message: 'Message successfully sent',
-        success: true,
-      });
-    } else if (req.method === 'GET') {
-      const { id } = req.query;
-      if (typeof id === 'string') {
-        const email = await Email.findOne({ id });
-        res.status(200).json(email);
+        await Email.create({
+          id: messageId,
+          name,
+          emailId: id,
+          subject,
+          message: htmlMessage,
+          senderEmail,
+          insights: {
+            sentiment: textInsights.sentiment,
+            keyPhrases: textInsights.keyPhrases,
+            priority: textInsights.priority,
+          },
+        });
+        res.status(200).json({
+          message: 'Message successfully sent',
+          success: true,
+        });
+        break;
       }
+      case 'GET': {
+        const { id } = req.query;
+        if (typeof id === 'string') {
+          const email = await Email.findOne({ id });
+          res.status(200).json(email);
+        }
+        break;
+      }
+      case 'PATCH': {
+        const { id, status } = req.query;
+
+        if (status === 'actionable' || status === 'resolved') {
+          await Email.findOneAndUpdate({ id: String(id) }, { status });
+          res
+            .status(200)
+            .json({ message: 'status successfully updated', success: true });
+        }
+        break;
+      }
+      case 'DELETE': {
+        const { id } = req.query;
+        await Email.findOneAndDelete({ id: String(id) });
+        res
+          .status(200)
+          .json({ message: 'Message succssfully deleted', success: true });
+        break;
+      }
+      default:
+        throw new Error('Unsupported Method');
     }
   } catch (error) {
     res
