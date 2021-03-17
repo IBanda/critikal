a60c3485fb3512cd7123583d238a9ee96c685a10
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-18 02:17:54 +0800

    add msw server

--- setupTests.ts
+++ setupTests.ts
@@ -1,2 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom/extend-expect';
+import { server } from 'mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
