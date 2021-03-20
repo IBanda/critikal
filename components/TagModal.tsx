efb35a3f894a0d41194e75ec355f20515244a8eb
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-21 02:49:06 +0800

    add tabs

--- components/TagModal.tsx
+++ components/TagModal.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import Modal from './Modal';
+import TagDeleteForm from './TagDeleteForm';
 import TagForm from './TagForm';
 
 interface Props {
@@ -6,9 +8,35 @@ interface Props {
   onHide: () => void;
 }
 export default function TagModal({ show, onHide }: Props) {
+  const [tab, setTab] = useState('add');
   return (
     <Modal show={show} onHide={onHide}>
-      <TagForm />
+      <div className="border-b border-blue-100">
+        <button
+          type="button"
+          onClick={() => setTab('add')}
+          className={`mr-1 relative z-10 tracking-tight rounded-tl rounded-tr -mb-0.5  p-1 focus:outline-none ${
+            tab === 'add' ? 'border border-mixed bg-white' : ''
+          }`}
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          onClick={() => setTab('delete')}
+          className={` tracking-tight rounded-tl rounded-tr -mb-0.5  focus:outline-none  p-1 ${
+            tab === 'delete' ? 'border border-mixed bg-white' : ''
+          }`}
+        >
+          Delete
+        </button>
+      </div>
+      <div className={`${tab === 'add' ? 'block' : 'hidden'}`}>
+        <TagForm />
+      </div>
+      <div className={`${tab === 'delete' ? 'block' : 'hidden'}`}>
+        <TagDeleteForm />
+      </div>
     </Modal>
   );
 }
