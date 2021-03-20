4611aabc923b251467675cf4fcb79b3e452167df
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-21 02:51:21 +0800

    use Tag instead

--- components/TagModalBtn.tsx
+++ components/TagModalBtn.tsx
@@ -11,7 +11,7 @@ export default function TagModalBtn() {
         onClick={() => setOnShowModal(true)}
         className="py-2 px-3  text-sm "
       >
-        <span className="hidden sm:inline">Key</span> Phrases
+        Tags
       </Button>
       {showModal && (
         <TagModal show={showModal} onHide={() => setOnShowModal(false)} />
