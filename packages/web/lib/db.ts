b96050cf1450388cd58f85b3f0438d4165faad11
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-26 05:06:25 +0800

    use atlas in prod

--- packages/web/lib/db.ts
+++ packages/web/lib/db.ts
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 
+let MONGODB_URI = 'mongodb://localhost:27017/critikalDB';
+if (process.env.NODE_ENV === 'production') {
+  MONGODB_URI = process.env.MONGODB_URI;
+}
 export default function db() {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-  return mongoose.connect(process.env.MONGODB_URI, {
+  return mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
