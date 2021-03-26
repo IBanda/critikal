d0eaafe0b1df3c2abdbf1be036bb19f42410ea92
Ian <38500123+IBanda@users.noreply.github.com>
2021-03-26 22:11:34 +0800

    add Datatable loading

---
+++ packages/web/components/TableLoading.tsx
@@ -0,0 +1,8 @@
+export default function TableLoading() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <img src="Processing.gif" alt="processing" />
+      <h3>Collecting your Messages...</h3>
+    </div>
+  );
+}
