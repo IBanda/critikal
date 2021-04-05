dac67577676f9ebad993f625b3bf91d01f02e62d
Ian <38500123+IBanda@users.noreply.github.com>
2021-04-05 12:44:22 +0800

    use includes util

--- packages/web/utils/modifyResult.ts
+++ packages/web/utils/modifyResult.ts
@@ -1,15 +1,12 @@
 import { TableData } from 'lib/interfaces';
+import includes from './includes';
 
 export default function modifyResult(tags: string[]) {
-  const tagsLength = tags.length;
+  // const tagsLength = tags.length;
   return (data: TableData[]) => {
     const copy = [...data];
     copy.forEach((dataItem) => {
-      const set = new Set([
-        ...tags.map((t) => t.toLowerCase()),
-        ...dataItem.keyPhrases.map((p) => p.toLowerCase()),
-      ]);
-      if (!(set.size === tagsLength + dataItem.keyPhrases.length)) {
+      if (includes(tags, dataItem.keyPhrases).length) {
         // eslint-disable-next-line no-param-reassign
         dataItem.hasListedTags = true;
       }
