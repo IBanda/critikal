2496891b01dd03c7b29da9e6bc6fe037cbe6d096
Ian <38500123+IBanda@users.noreply.github.com>
2021-04-05 12:50:48 +0800

    add textAnalytics mock

--- packages/web/setupTests.ts
+++ packages/web/setupTests.ts
@@ -1,7 +1,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom/extend-expect';
 import { server } from 'mocks/server';
+import { loadEnvConfig } from '@next/env';
+
+jest.mock('lib/textAnalytics', () => ({
+  __esModule: true,
+  default: jest.fn((message) => ({
+    sentiment: 'negative',
+    priority: 'high',
+    sentences: [message],
+    opnions: [{ text: 'service', opinion: 'dissappointed' }],
+    keyPhrases: ['service', 'disappointed'],
+  })),
+}));
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+export default async () => {
+  const projectDir = process.cwd();
+  loadEnvConfig(projectDir);
+};
